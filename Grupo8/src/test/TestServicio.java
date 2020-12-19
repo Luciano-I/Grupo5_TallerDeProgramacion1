8c6719a92519cd1d98b9281c1c370bb3d18f5398
darksoul96 <62730262+darksoul96@users.noreply.github.com>
2020-12-18 21:21:58 -0300

    Add Test para Internet100/500 (Constructor)

--- Grupo8/src/test/TestServicio.java
+++ Grupo8/src/test/TestServicio.java
@@ -3,8 +3,15 @@ package test;
 import static org.junit.Assert.*;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import servicios.Domicilio;
+import servicios.DomicilioCasa;
+import servicios.Internet100;
+import servicios.Internet500;
+import servicios.Servicio;
 
 public class TestServicio {
 
@@ -17,8 +24,40 @@ public class TestServicio {
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testConstructorInternet100() {
+		Domicilio domicilio=new DomicilioCasa("asafas",1212);
+		Servicio servicio=new Internet100(domicilio);
+
+		Assert.assertEquals("El domicilio se creo correctamente en el Servicio de internet100", domicilio, servicio.getDomicilio());
 	}
+	
+	@Test
+	public void testConstructor2Internet100() {
+		Domicilio domicilio=new DomicilioCasa("asafas",1212);
+		Servicio servicio=new Internet100(domicilio, 5);
+		int id=servicio.getID();
+		Assert.assertEquals("El domicilio se creo correctamente en el Servicio de internet100", domicilio, servicio.getDomicilio());
+		Assert.assertEquals("El id se creo correctamente en internet100", 5, id);
+	}
+	
+	@Test
+	public void testConstructorInternet500() {
+		Domicilio domicilio=new DomicilioCasa("asafas",1212);
+		Servicio servicio=new Internet500(domicilio);
+
+		Assert.assertEquals("El domicilio se creo correctamente en el Servicio de internet500", domicilio, servicio.getDomicilio());
+	}
+	
+	
+	@Test
+	public void testConstructor2Internet500() {
+		Domicilio domicilio=new DomicilioCasa("asafas",1212);
+		Servicio servicio=new Internet500(domicilio, 5);
+		int id=servicio.getID();
+		Assert.assertEquals("El domicilio se creo correctamente en el Servicio de internet100", domicilio, servicio.getDomicilio());
+		Assert.assertEquals("El id se creo correctamente en internet100", 5, id);
+	}
+	
+	
 
 }
