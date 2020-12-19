5cd2b1326aa05112f2ca3df3fe655bcd500ac2d7
darksoul96 <62730262+darksoul96@users.noreply.github.com>
2020-12-19 19:24:49 -0300

    Add EscenarioFacturaConDatos

--- Grupo8/src/test/EscenarioFacturaConDatos.java
+++ Grupo8/src/test/EscenarioFacturaConDatos.java
@@ -1,5 +1,6 @@
 package test;
 
+import agregado.Celular;
 import personas.Fisica;
 import servicios.DomicilioCasa;
 import servicios.Factura;
@@ -10,7 +11,9 @@ public class EscenarioFacturaConDatos {
 	
 	{
 		factura=new Factura(new Fisica("Jorge",14125235));
-		factura.nuevaContratacion(new Internet100(new DomicilioCasa("aaagasga",1212)));
+		Internet100 internet100=new Internet100(new DomicilioCasa("aaagasga",1212));
+		factura.nuevaContratacion(internet100);
+		factura.nuevaContratacion(new Celular(3, internet100));
 	}
 
 }
